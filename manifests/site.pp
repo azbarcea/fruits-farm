d962aad15c647cac39b17ecb356de741f2d32309
ezabolo <eugene.zabolo@gmail.com>
2015-01-29 11:05:34 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,13 +1,6 @@
 include all
 
-node 'becks.swift.com', 'coors.swift.com', 'bluemoon.swift.com' {
-      include schedule
-      include apache
-}
 
-node 'tap.swift.com' {
-     include graphite
-}
 
 
 
